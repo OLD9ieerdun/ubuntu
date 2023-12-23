17e620896d9c08fbdbdf6dd2b600cdab3e724605
OLD_Pieerdun <107999480+OLD9ieerdun@users.noreply.github.com>
2023-12-23 12:19:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-// Import the express module
 const express = require('express');
-
-// Create an instance of express
 const app = express();
-
-// Define a route
 app.get('/', (req, res) => {
   res.send('Shrek is forever alone');
 });
-
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
